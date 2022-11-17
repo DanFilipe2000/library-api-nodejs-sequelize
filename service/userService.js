e55d2d9ad386a8e398d8412363185400be1c6fae
Daniel Filipe <anielilipe90@gmail.com>
2022-11-17 11:07:28 -0300

    adiciona algumas explicações ao arquivo userService

--- service/userService.js
+++ service/userService.js
@@ -2,10 +2,10 @@ const { User } = require('../database/models');
 const jwt = require('../jwt/auth');
 
 const login = async ({ email, password }) => {
-  const data = await User.findOne({ where: { email }});
-  if (!data || data.dataValues.password !== password) throw new Error('Senha Inválida');
-  const token = jwt.createToken(data.dataValues.name);
-  return token;
+  const data = await User.findOne({ where: { email }}); // Faz a query pro banco de dados e retorna o usuário de acordo com o email passado
+  if (!data || data.dataValues.password !== password) throw new Error('Senha inválida ou usuário inexistente'); // Caso o usuário não exista ou senha for inválida dispara um erro
+  const token = jwt.createToken(data.dataValues.name); // Cria um token para autenticar nas demais rotas
+  return token; // Retorna o token para ser utilizado na header das outras rotas
 }
 
 module.exports = {
