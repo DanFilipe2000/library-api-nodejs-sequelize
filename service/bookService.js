a3c990bba385e217d22ae8f77e02f0fda40fa68b
Daniel Filipe <anielilipe90@gmail.com>
2022-11-19 13:06:55 -0300

    fix: remove pasta app desnecessária

--- service/bookService.js
+++ service/bookService.js
@@ -1,7 +1,7 @@
 const { Book } = require('../database/models');
 
 const create = async ({ isbn, nome, autor, editora, ano_de_publicacao, disponivel }) => {
-	const verify = await Book.findOne({ where: { nome } });
+	const verify = await Book.findOne({ where: { isbn } });
 	if (verify) throw new Error('O Livro já foi cadastrado');
   const result = await Book.create({ isbn, nome, autor, editora, ano_de_publicacao, disponivel });
 	return result;
