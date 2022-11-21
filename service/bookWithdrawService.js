17a93c1b1dbe8f5d134f59a6dfbb5ce82f3d57e9
Daniel Filipe <anielilipe90@gmail.com>
2022-11-21 13:25:25 -0300

    feat: add verificação na rota book/withdraw

--- service/bookWithdrawService.js
+++ service/bookWithdrawService.js
@@ -1,10 +1,14 @@
 const { Book, BookWithdraw } = require('../database/models');
 
 const createAndUpdate = async ({ clientId, bookId }) => {
+  const verify = await BookWithdraw.findOne({ where: { bookId } });
+  const verifyBook = await Book.findOne({ where: { id: bookId } });
+  if (!verifyBook) throw new Error('O livro não existe');
+  if (verify) throw new Error('O livro já foi retirado na biblioteca');
   const dataDeDevolução = new Date();
   dataDeDevolução.setDate(dataDeDevolução.getDate() + 7);
   const bookWithdrawCreate = await BookWithdraw.create({ clientId, bookId, dataDeDevolução});
-  await Book.update({ disponivel: false }, { where: { id: bookId }, returning: true});
+  await Book.update({ disponivel: false }, { where: { id: bookId }});
   return bookWithdrawCreate.dataDeDevolução;
 };
 
--- service/clientService.js
+++ service/clientService.js
@@ -3,7 +3,7 @@ const { Client } = require('../database/models');
 const create = async ({ nome, matricula, telefone }) => {
 	const verify = await Client.findOne({ where: { matricula } });
 	if (verify) throw new Error('O cliente já foi cadastrado');
-  const result = await Client.create({ nome, matricula, telefone });
+  	const result = await Client.create({ nome, matricula, telefone });
 	return result;
 };
 
